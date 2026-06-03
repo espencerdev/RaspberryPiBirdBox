4cf830156d775864c558a7428e41276b1ae469cb
Edward Spencer <96989514+espencerdev@users.noreply.github.com>
2026-06-03 11:59:11 +0100

    Update vunerable npm modules and fix install script variable

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-OLDPWD=$PWD
+ORIGINAL_PWD=$PWD
 sudo cp ./eth-wifi-switch.sh /usr/local/bin/eth-wifi-switch.sh
 sudo chmod +x /usr/local/bin/eth-wifi-switch.sh
 sudo cp ./eth-wifi-switch.service /etc/systemd/system/eth-wifi-switch.service
@@ -12,15 +12,14 @@ sudo mkdir /usr/local/bin/mediamtx
 cd /usr/local/bin/mediamtx
 sudo wget https://github.com/bluenviron/mediamtx/releases/download/v1.14.0/mediamtx_v1.14.0_linux_arm64.tar.gz
 sudo tar xzf mediamtx_v1.14.0_linux_arm64.tar.gz
-cd $OLDPWD
+cd $ORIGINAL_PWD
 sudo cp ./mediamtx.service /etc/systemd/system/mediamtx.service
 sudo cp ./mediamtx.yml /usr/local/bin/mediamtx/
 
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 \. "$HOME/.nvm/nvm.sh"
 nvm install 24
-cd $OLDPWD
-cd ./bird-interface/backend/
+cd $ORIGINAL_PWD/bird-interface/backend/
 npm i
 cd ../frontend/
 npm i
